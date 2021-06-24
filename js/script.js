a27fcd5a075c189dcfec4a609511fb2cf1df5aa1
Ruslan <nateriver345@gmail.com>
2021-06-24 22:13:17 +0300

    delete PHP mailer

--- js/script.js
+++ js/script.js
@@ -180,116 +180,116 @@ document.addEventListener('keydown', function (e) {
 
 //Отправка формы
 
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('form')
-  form.addEventListener('submit', formSend)
+// document.addEventListener('DOMContentLoaded', function () {
+//   const form = document.getElementById('form')
+//   form.addEventListener('submit', formSend)
 
-  async function formSend(e) {
-    e.preventDefault()
+//   async function formSend(e) {
+//     e.preventDefault()
 
-    let error = formValidate(form)
+//     let error = formValidate(form)
 
-    let formData = new FormData(form)
-    formData.append('image', formImage.files[0])
+//     let formData = new FormData(form)
+//     formData.append('image', formImage.files[0])
 
-    if (error === 0) {
-      form.classList.add('_sending')
-      //AJAX запрос
-      let response = await fetch('sendmail.php', {
-        method: 'POST',
-        body: formData,
-      })
+//     if (error === 0) {
+//       form.classList.add('_sending')
+//       //AJAX запрос
+//       let response = await fetch('sendmail.php', {
+//         method: 'POST',
+//         body: formData,
+//       })
 
-      if (response.ok) {
-        let result = await response.json()
-        alert(result.message)
-        formPreview.innerHTML = ''
-        form.reset()
-        form.classList.remove('_sending')
-      } else {
-        alert('Ошибка!')
-        form.classList.remove('_sending')
-      }
-    } else {
-      alert('Заполните обязательные поля!')
-    }
-  }
+//       if (response.ok) {
+//         let result = await response.json()
+//         alert(result.message)
+//         formPreview.innerHTML = ''
+//         form.reset()
+//         form.classList.remove('_sending')
+//       } else {
+//         alert('Ошибка!')
+//         form.classList.remove('_sending')
+//       }
+//     } else {
+//       alert('Заполните обязательные поля!')
+//     }
+//   }
 
-  function formValidate(form) {
-    let error = 0
-    let formReq = document.querySelectorAll('._req')
+//   function formValidate(form) {
+//     let error = 0
+//     let formReq = document.querySelectorAll('._req')
 
-    for (let index = 0; index < formReq.length; index++) {
-      const input = formReq[index]
-      formRemoveError(input)
+//     for (let index = 0; index < formReq.length; index++) {
+//       const input = formReq[index]
+//       formRemoveError(input)
 
-      if (input.classList.contains('_email')) {
-        if (emailTest(input)) {
-          formAddError(input)
-          error++
-        }
-      } else if (
-        input.getAttribute('type') === 'checkbox' &&
-        input.checked === false
-      ) {
-        formAddError(input)
-        error++
-      } else {
-        if (input.value === '') {
-          formAddError(input)
-          error++
-        }
-      }
-    }
-    return error
-  }
+//       if (input.classList.contains('_email')) {
+//         if (emailTest(input)) {
+//           formAddError(input)
+//           error++
+//         }
+//       } else if (
+//         input.getAttribute('type') === 'checkbox' &&
+//         input.checked === false
+//       ) {
+//         formAddError(input)
+//         error++
+//       } else {
+//         if (input.value === '') {
+//           formAddError(input)
+//           error++
+//         }
+//       }
+//     }
+//     return error
+//   }
 
-  function formAddError(input) {
-    input.parentElement.classList.add('_error')
-    input.classList.add('_error')
-  }
-  function formRemoveError(input) {
-    input.parentElement.classList.remove('_error')
-    input.classList.remove('_error')
-  }
-  //Функция для проверки email
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
-  }
+//   function formAddError(input) {
+//     input.parentElement.classList.add('_error')
+//     input.classList.add('_error')
+//   }
+//   function formRemoveError(input) {
+//     input.parentElement.classList.remove('_error')
+//     input.classList.remove('_error')
+//   }
+//   //Функция для проверки email
+//   function emailTest(input) {
+//     return !/^\w+([\.-]?\w)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
+//   }
 
-  //Функция превью картинки
-  //Получаем input file в переменную
-  const formImage = document.getElementById('formImage')
-  //Получаем div для превью в переменную
-  const formPreview = document.getElementById('formPreview')
-  //Слушаем изменения в input file
-  formImage.addEventListener('change', () => {
-    uploadFile(formImage.files[0])
-  })
+//   //Функция превью картинки
+//   //Получаем input file в переменную
+//   const formImage = document.getElementById('formImage')
+//   //Получаем div для превью в переменную
+//   const formPreview = document.getElementById('formPreview')
+//   //Слушаем изменения в input file
+//   formImage.addEventListener('change', () => {
+//     uploadFile(formImage.files[0])
+//   })
 
-  function uploadFile(file) {
-    //Проверка типа файла
-    if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
-      alert('Разрешены только изображения!')
-      formImage.value = ''
-      return
-    }
-    //Проверка размера файла(<2 Мб)
-    if (file.size > 2 * 1024 * 1024) {
-      alert('Файл должен быть менее 2 МБ!')
-      return
-    }
+//   function uploadFile(file) {
+//     //Проверка типа файла
+//     if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+//       alert('Разрешены только изображения!')
+//       formImage.value = ''
+//       return
+//     }
+//     //Проверка размера файла(<2 Мб)
+//     if (file.size > 2 * 1024 * 1024) {
+//       alert('Файл должен быть менее 2 МБ!')
+//       return
+//     }
 
-    var reader = new FileReader()
-    reader.onload = function (e) {
-      formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`
-    }
-    reader.onerror = function (e) {
-      alert('Ошибка')
-    }
-    reader.readAsDataURL(file)
-  }
-})
+//     var reader = new FileReader()
+//     reader.onload = function (e) {
+//       formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`
+//     }
+//     reader.onerror = function (e) {
+//       alert('Ошибка')
+//     }
+//     reader.readAsDataURL(file)
+//   }
+// })
 
 //Аккордион
 function accordion() {
